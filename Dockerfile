3d2744e5e9a5d5319043079efba727b7ca71cb04
anantkrsingh <anantkrplay@gmail.com>
2023-07-04 10:29:34 +0530

    Added-Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM node:16.3.0-alpine
+WORKDIR app
+COPY . .
+RUN npm install
+EXPOSE 4040
+CMD ["npm","start"]
--- src/index.js
+++ src/index.js
@@ -1,71 +1,54 @@
-const express = require('express');
-const cors = require('cors');
-const http = require('http')
-const mongoose = require('mongoose');
-const env = require('dotenv');
-const bodyParser = require('body-parser');
-const socketIO = require('socket.io');
-const {Server} = require('socket.io')
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const mongoose = require("mongoose");
+const env = require("dotenv");
+const bodyParser = require("body-parser");
+const socketIO = require("socket.io");
+const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server)
-
-
-
-
+const io = socketIO(server);
 
 env.config();
-const authRoutes = require('./Routes/Auth');
-const roomRoutes = require('./Routes/Generator')
-const sessionRoutes = require('./Routes/Session')
-const { createMeeting } = require('./Controllers/Generator');
-
-
+const authRoutes = require("./Routes/Auth");
+const roomRoutes = require("./Routes/Generator");
+const sessionRoutes = require("./Routes/Session");
+const { createMeeting } = require("./Controllers/Generator");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(cors())
-io.on('connection' , (socket) =>{
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+io.on("connection", (socket) => {
   console.log("Client Connected");
 
-  socket.on('disconnect', () =>{
+  socket.on("disconnect", () => {
     console.log("Client Disconnected");
-  })
+  });
 
-  socket.on('chat message', msg => {
+  socket.on("chat message", (msg) => {
     console.log(msg);
   });
-})
-app.use('/api',authRoutes);
-app.use('/api/meeting',roomRoutes)
+});
+app.use("/api", authRoutes);
+app.use("/api/meeting", roomRoutes);
 
-app.use("/api/session",sessionRoutes)
+app.use("/api/session", sessionRoutes);
 
-
-
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
   console.log(req.query.name);
-    res.send(req.query.name);
-})
-
-
-
-
-server.listen("4040",()=>{
-  console.log('Server Started port = 4040');
+  res.send(req.query.name);
 });
 
-
+server.listen("4040", () => {
+  console.log("Server Started port = 4040");
+});
 
 mongoose
-  .connect(
-    `mongodb://127.0.0.1:27017/audio-db`
-  )
+  .connect(`mongodb://127.0.0.1:27017/audio-db`)
   .then(() => {
     console.log("DB Connected");
-  }).catch((error)=>{
-    console.log(error)
+  })
+  .catch((error) => {
+    console.log(error);
   });
-
-
-  
